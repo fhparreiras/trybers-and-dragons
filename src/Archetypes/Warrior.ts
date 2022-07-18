37c253537ff0371ea02c519b925e77c86081762f
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2022-07-18 16:10:12 -0300

    feat:req5

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,23 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Warrior extends Archetype {
+  private _energyType: EnergyType;
+  private static _instancesCount = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    this._instancesCount += 1;
+    return this._instancesCount;
+  }
+}
+
+export default Warrior;
